65259b8fa158c11b3b6ab4bcd8990a3a101f7228
vagrant <llozanop2003@gmail.com>
2025-03-12 23:42:48 +0000

    Bug de migraciones de correcciones

--- database/migrations/2025_03_12_185456_add_curso_id_to_correcciones_table.php
+++ database/migrations/2025_03_12_185456_add_curso_id_to_correcciones_table.php
@@ -13,14 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('correccions', function (Blueprint $table) {
+        Schema::table('correcciones', function (Blueprint $table) {
             $table->foreignId('curso_id')->constrained('cursos')->onDelete('cascade');
         });
     }
 
     public function down()
     {
-        Schema::table('correccions', function (Blueprint $table) {
+        Schema::table('correcciones', function (Blueprint $table) {
             $table->dropForeign(['curso_id']);
             $table->dropColumn('curso_id');
         });
